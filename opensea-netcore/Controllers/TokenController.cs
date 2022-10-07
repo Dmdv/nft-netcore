ef0dd0f530f28861036b25e27124d6db379e25b3
DmitryD <dima.dyachkov@gmail.com>
2022-10-07 19:23:14 +0800

    Opensea endpoint

---
+++ opensea-netcore/Controllers/TokenController.cs
@@ -0,0 +1,38 @@
+using Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace Opensea.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TokenController : ControllerBase, IDisposable
+{
+    private readonly ILogger<TokenController> _logger;
+    private readonly HttpClient _httpClient;
+
+    public TokenController(ILogger<TokenController> logger, IHttpClientFactory clientFactory)
+    {
+        _logger = logger;
+        _httpClient = clientFactory.CreateClient("opensea");
+        _logger.LogInformation("Started Token controller");
+    }
+
+    // GET: api/token/5/5
+    [HttpGet("{assetContractAddress}/{tokenId}", Name = "Get")]
+    [ProducesResponseType(StatusCodes.Status200OK , Type = typeof(Token))]
+    public async Task<IResult> Get(string assetContractAddress, string tokenId)
+    {
+        _logger.LogInformation("Token: {AssetContractAddress} and {TokenId}", assetContractAddress, tokenId);
+        
+        var url = $"api/v1/asset/{assetContractAddress}/{tokenId}/";
+        var json = await _httpClient.GetStringAsync(url);
+        
+        return Results.Json(json);
+    }
+
+    public void Dispose()
+    {
+        _httpClient.Dispose();
+    }
+}
---
+++ opensea-netcore/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+
+builder.Services.AddLogging();
+builder.Services.AddMemoryCache();
+builder.Services.AddHttpClient("opensea", c =>
+{
+    c.BaseAddress = new Uri("https://api.opensea.io/");
+    c.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+    c.DefaultRequestHeaders.Add(HeaderNames.UserAgent, "centralex");
+    c.DefaultRequestHeaders.Add("X-API-KEY", "YOUR-TOKEN-HERE");
+});
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days.
+    // You may want to change this for production scenarios,
+    // see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpLogging();
+app.UseHttpsRedirection();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = 
+        ForwardedHeaders.XForwardedFor | 
+        ForwardedHeaders.XForwardedHost | 
+        ForwardedHeaders.XForwardedProto
+});
+
+// app.UseAuthorization();
+app.MapControllers();
+
+app.MapGet("/", () => "This is the root of opensea service");
+
+app.Run();
